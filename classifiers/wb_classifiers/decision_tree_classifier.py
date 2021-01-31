1541bee18172eae07bfa58de37a491cecee5eb9b
ThanasisTs <thtsitos@gmail.com>
2021-02-01 00:25:20 +0200

    added confusion matrices

--- classifiers/wb_classifiers/decision_tree_classifier.py
+++ classifiers/wb_classifiers/decision_tree_classifier.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 dataset = pd.read_csv(sys.argv[1])
@@ -12,8 +12,10 @@ dataset = pd.read_csv(sys.argv[1])
 X = dataset[dataset.columns[1:len(dataset.columns)-1]].values
 y = dataset['Class'].values
 
-kf = KFold(n_splits=20, shuffle=False, random_state=None)
+kf = KFold(n_splits=10, shuffle=False, random_state=None)
 avg_acc = 0.0
+conf_matrix = np.array([0 for i in range(25)])
+conf_matrix = conf_matrix.reshape(5,5)
 
 for train_index, test_index in kf.split(X):
 	X_train, X_test = X[train_index], X[test_index]
@@ -21,8 +23,10 @@ for train_index, test_index in kf.split(X):
 
 	clf = DecisionTreeClassifier()
 	clf.fit(X_train, y_train)
-
 	y_pred = clf.predict(X_test)
-	avg_acc += accuracy_score(y_test, y_pred)/20
+	conf_matrix += confusion_matrix(y_test, y_pred)
+	avg_acc += accuracy_score(y_test, y_pred)/10
+
 
 print(avg_acc*100)
+print(conf_matrix)
--- classifiers/xy_classifiers/decision_tree_classifier.py
+++ classifiers/xy_classifiers/decision_tree_classifier.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import  KFold
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 dataset = pd.read_csv(sys.argv[1])
@@ -12,8 +12,10 @@ dataset = pd.read_csv(sys.argv[1])
 X = dataset[dataset.columns[1:len(dataset.columns)-1]].values
 y = dataset['Class'].values
 
-kf = KFold(n_splits=20, shuffle=False, random_state=None)
-avg_acc = [0.0 for i in range(20)]
+kf = KFold(n_splits=10, shuffle=False, random_state=None)
+avg_acc = [0.0 for i in range(10)]
+conf_matrix = [np.array([0 for i in range(25)]) for i in range(10)]
+conf_matrix = [i.reshape(5,5) for i in conf_matrix]
 
 for train_index, test_index in kf.split(X):
 	X_train, X_test = X[train_index], X[test_index]
@@ -26,9 +28,18 @@ for train_index, test_index in kf.split(X):
 
 		y_pred = clf.predict(X_test[:,:i])
 		scores.update({i//2 : accuracy_score(y_test, y_pred)*100})
-		avg_acc[i//2] += scores.get(i//2)/20
+		try:
+			conf_matrix[i//2-1] += confusion_matrix(y_test, y_pred)
+		except:
+			pass
+		avg_acc[i//2-1] += scores.get(i//2)/10
 
 keys = [i for i in scores.keys()]
 for k in range(len(scores.keys())):
 	print(keys[k],str(round(avg_acc[k],2))+'%')
 
+j=2
+for i in conf_matrix:
+	print("Number of pixels: {}".format(j))
+	print(i)
+	j += 2
