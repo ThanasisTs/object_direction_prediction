787593edab717e4d2daf7829fc539e8145284759
ThanasisTs <thtsitos@gmail.com>
2021-01-31 23:24:19 +0200

    KFold in classification

--- classifiers/wb_classifiers/decision_tree_classifier.py
+++ classifiers/wb_classifiers/decision_tree_classifier.py
@@ -3,19 +3,26 @@ import numpy as np
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 
 
 dataset = pd.read_csv(sys.argv[1])
 
-X = dataset[dataset.columns[1:len(dataset.columns)-1]]
-y = dataset['Class']
+X = dataset[dataset.columns[1:len(dataset.columns)-1]].values
+y = dataset['Class'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+kf = KFold(n_splits=20, shuffle=False, random_state=None)
+avg_acc = 0.0
 
-clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
+for train_index, test_index in kf.split(X):
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]	
 
-y_pred = clf.predict(X_test)
-print(accuracy_score(y_test, y_pred)*100)
+	clf = DecisionTreeClassifier()
+	clf.fit(X_train, y_train)
+
+	y_pred = clf.predict(X_test)
+	avg_acc += accuracy_score(y_test, y_pred)/20
+
+print(avg_acc*100)
--- classifiers/xy_classifiers/decision_tree_classifier.py
+++ classifiers/xy_classifiers/decision_tree_classifier.py
@@ -3,24 +3,32 @@ import numpy as np
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import  KFold
 from sklearn.metrics import accuracy_score
 
 
 dataset = pd.read_csv(sys.argv[1])
 
-X = dataset[dataset.columns[1:len(dataset.columns)-1]]
-y = dataset['Class']
+X = dataset[dataset.columns[1:len(dataset.columns)-1]].values
+y = dataset['Class'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+kf = KFold(n_splits=20, shuffle=False, random_state=None)
+avg_acc = [0.0 for i in range(20)]
 
-clf = DecisionTreeClassifier()
-scores = {}
-for i in range(2, len(X_train.columns)+1, 2):
-	clf.fit(X_train[X_train.columns[:i]], y_train)
+for train_index, test_index in kf.split(X):
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]	
 
-	y_pred = clf.predict(X_test[X_train.columns[:i]])
-	scores.update({i//2 : accuracy_score(y_test, y_pred)*100})
+	clf = DecisionTreeClassifier()
+	scores = {}
+	for i in range(2, len(X_train[0])+1, 2):
+		clf.fit(X_train[:,:i], y_train)
 
-for k,v in scores.items():
-	print(k,str(round(v,2))+'%')
+		y_pred = clf.predict(X_test[:,:i])
+		scores.update({i//2 : accuracy_score(y_test, y_pred)*100})
+		avg_acc[i//2] += scores.get(i//2)/20
+
+keys = [i for i in scores.keys()]
+for k in range(len(scores.keys())):
+	print(keys[k],str(round(avg_acc[k],2))+'%')
+
