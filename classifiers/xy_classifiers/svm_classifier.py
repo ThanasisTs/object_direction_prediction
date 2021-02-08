fd88b32b85af33826c8b76515bd045fc56e24c4d
ThanasisTs <thtsitos@gmail.com>
2021-02-08 10:59:32 +0200

    scaling and parameter tuning for svm

--- classifiers/xy_classifiers/svm_classifier.py
+++ classifiers/xy_classifiers/svm_classifier.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.svm import SVC
-from sklearn.model_selection import  KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 dataset = pd.read_csv(sys.argv[1])
@@ -13,16 +14,26 @@ n_splits = 10
 X = dataset[dataset.columns[1:len(dataset.columns)-1]].values
 y = dataset['Class'].values
 
+
 kf = KFold(n_splits=n_splits, shuffle=False, random_state=None)
 avg_acc = [0.0 for i in range(num_points)]
 conf_matrix = [np.array([0 for i in range(25)]) for i in range(num_points)]
 conf_matrix = [i.reshape(5,5) for i in conf_matrix]
 
+parameters = {'kernel' : ('linear', 'rbf'), 'C' : [1, 10]}
+
 for train_index, test_index in kf.split(X):
 	X_train, X_test = X[train_index], X[test_index]
 	y_train, y_test = y[train_index], y[test_index]	
+	# st_x = StandardScaler()
+	# st_y = StandardScaler()
+
+	# X = st_x.fit_transform(X)
+	# y = st_y.fit_transform(y.reshape(-1,1))
 
 	clf = SVC()
+	# svc = SVC()
+	# clf = GridSearchCV(svc, parameters)
 	scores = {}
 	
 	for i in range(2, len(X_train[0])+1, 2):
@@ -42,8 +53,8 @@ print("Number of points | Accuracy")
 for k in range(len(scores.keys())):
 	print("{}     		 |    {}%".format(keys[k]*2,str(round(avg_acc[k],2))))
 
-j=2
-for i in conf_matrix:
-	print("Number of pixels: {}".format(j))
-	print(i)
-	j += 2
+# j=2
+# for i in conf_matrix:
+# 	print("Number of pixels: {}".format(j))
+# 	print(i)
+# 	j += 2
